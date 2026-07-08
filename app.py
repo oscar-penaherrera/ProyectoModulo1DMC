7c555e70c0228da7410755f3d89dd5fd145fbd02
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-07 22:29:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ valor_inicial = st.number_input("Ingrese valor inicial", value=0)
 valor_final = st.number_input("Ingrese valor final", value=1)
 
 lista_numerica = list(range(valor_inicial,valor_final))
+
+st.write (lista_numerica)
