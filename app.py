Fix syntax for displaying production totals and averages

app.py
@@ -25,8 +25,8 @@ elif modulo == "Módulo Array":
 
   st.write (datos_produccion)
 
-  st.write("La producción total es:" np.sum(datos_produccion))
-  st.write("La producción promedio es:" np.mean(datos_produccion))
+  st.write("La producción total es:", np.sum(datos_produccion))
+  st.write("La producción promedio es:", np.mean(datos_produccion))
   
 else:
   st.write("Estas en el módulo de funciones")