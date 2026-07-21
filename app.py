2bf1eadb58f15ab45508fcbdaeb114759451eba9
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-20 23:34:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ st.title("Proyecto Modulo 01 Fundamentals")
 st.sidebar.title("Parámetros")
 
 st.image("Python_logo.png")
-
 st.sidebar.image("DMC.png")
+
+modulo  =  st.sidebar.selectbox("Elija un módulo",["Módulo Listas","Módulo Array","Módulo Funciones"])
+
+if modulo ==  "Módulo Listas"
+
 valor_inicial = st.number_input("Ingrese valor inicial", value=0)
 valor_final = st.number_input("Ingrese valor final", value=1)
-
 lista_numerica = list(range(valor_inicial,valor_final))
-
 st.write (lista_numerica)
+
+elif modulo == "Módulo Array":
+  st.write("Estas en el módulo de arreglos)
+
+else:
+  st.write("Estas en el módulo de funciones)
